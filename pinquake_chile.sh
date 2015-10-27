181c3700ad4fab4f96fbce65ce3e89ab5a2f9ad4
cont3mpo <cont3mpo@gmail.com>
2015-10-27 15:26:49 -0300

    Update pinquake_chile.sh

--- pinquake_chile.sh
+++ pinquake_chile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 terminal-notifier -title "Pinquake (Chile)" -message "Esperando sismo para notificar..."
-echo 'Pinquake (Chile) - Esperando sismo para notificar...'
+echo "Pinquake (Chile) - Esperando sismo para notificar..."
 touch /tmp/nuevo_chile /tmp/viejo_chile
-while sleep 1m; (curl -s http://sismologia.cl/links/tabla.html | sed -n '26,40p' | cut -d '>' -f 4,7,9 | sed 's|</a>| - |g ; s|</td>| - |g ; s|Ml[^>]*||g' > /tmp/nuevo_chile); do diff /tmp/nuevo_chile /tmp/viejo_chile | head -n 1; done | while read line; do terminal-notifier -title "Pinquake (Chile)" -message "`head -n 1 /tmp/nuevo_chile`"; cp /tmp/nuevo_chile /tmp/viejo_chile; done
+while sleep 1m; (curl -s http://sismologia.cl/links/tabla.html | sed -n '26,40p' | cut -d '>' -f 4,7,9 | sed 's|</a>| - |g ; s|</td>| - |g ; s|Ml[^>]*||g' > /tmp/nuevo_chile); do diff /tmp/nuevo_chile /tmp/viejo_chile | head -n 1; done | while read line; do terminal-notifier -title "Pinquake (Chile)" -message "`head -n 1 /tmp/nuevo_chile`" ; echo "`head -n 1 /tmp/nuevo_chile`" ; cp /tmp/nuevo_chile /tmp/viejo_chile; done
