85c14ddf02240ec277cf7ba4fa3be0962df432b8
cont3mpo <cont3mpo@gmail.com>
2015-10-27 15:28:49 -0300

    Update pinquake_japon.sh

--- pinquake_japon.sh
+++ pinquake_japon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 terminal-notifier -title "Pinquake (Japón)" -message "Esperando sismo para notificar..."
-echo 'Pinquake (Japón) - Esperando sismo para notificar...'
+echo "Pinquake (Japón) - Esperando sismo para notificar..."
 touch /tmp/nuevo_japon /tmp/viejo_japon
-while sleep 1m; (curl -s http://www.jma.go.jp/en/quake/quake_local_index.html | sed -n '187,225p' | cut -d '>' -f 7,9,11 | sed 's| JST||g ; s|</td>| - |g ; s|</td||g' > /tmp/nuevo_japon); do (diff /tmp/nuevo_japon /tmp/viejo_japon | head -n 1); done | while read line; do (terminal-notifier -title "Pinquake (Japón)" -message "`head -n 1 /tmp/nuevo_japon`"); cp /tmp/nuevo_japon /tmp/viejo_japon; done
+while sleep 1m; (curl -s http://www.jma.go.jp/en/quake/quake_local_index.html | sed -n '187,225p' | cut -d '>' -f 7,9,11 | sed 's| JST||g ; s|</td>| - |g ; s|</td||g' > /tmp/nuevo_japon); do (diff /tmp/nuevo_japon /tmp/viejo_japon | head -n 1); done | while read line; do (terminal-notifier -title "Pinquake (Japón)" -message "`head -n 1 /tmp/nuevo_japon`") ; echo "`head -n 1 /tmp/nuevo_japon`" ; cp /tmp/nuevo_japon /tmp/viejo_japon; done
